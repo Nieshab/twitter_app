70760614a38e7bf99da441775cf24c329661904c
Niesha Butler <niesha@sportsdbn.com>
2017-04-26 12:56:20 -0400

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+         has_many :tweets
+
+          validates :username, presence: true, uniqueness: true
+# this is where the array that contains everyone you are following in
+          serialize :following, Array
+
+
+
+end
